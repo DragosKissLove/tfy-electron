c51ef2b8b728f5e73e833a8adefbd6cb32fd45e6
.regele <vi9lomister@gmail.com>
2025-05-25 15:59:04 +0100

    Update core functionality and dependencies

--- preload.js
+++ preload.js
@@ -3,5 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('electron', {
   runFunction: (name, args) => ipcRenderer.invoke('run-function', { name, args }),
   getSettings: () => ipcRenderer.invoke('get-settings'),
-  saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings)
+  saveSettings: (settings) => ipcRenderer.invoke('save-settings', settings),
+  platform: process.platform
 });
